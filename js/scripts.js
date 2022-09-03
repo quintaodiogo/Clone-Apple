ad7794fdf6414ea03665d38c80f14941dd1ce7a4
Diogo Quintão <quintaodiogo@gmail.com>
2022-09-03 20:12:55 -0300

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-// WORKING//
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector("#product-image");
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        console.log(e);
+
+        buttons.forEach((btn) =>
+            btn.querySelector(".color").classList.remove("selected")
+        );
+        buttons.forEach((btn) =>
+            btn.querySelector(".description").classList.remove("active")
+        );
+
+        const button = e.target;
+
+        const id = button.getAttribute("id");
+
+        button.querySelector(".color").classList.add("selected");
+        button.querySelector('.description').classList.add("active");
+        image.classList.toggle("changing");
+        image.setAttribute("src", `img/iphone_${id}.jpg`);
+
+        setTimeout(() => {
+            image.classList.toggle("changing");
+        }, 100);
+    });
+});
